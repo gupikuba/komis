95c31e486cbd2c232f6066dc6ca34874ec0d2dca
student <fjd@gmai.com>
2019-11-24 14:13:12 +0100

    komis 2

---
+++ src/main/java/pl/altkom/web/ControllerServlet.java
@@ -0,0 +1,29 @@
+package pl.altkom.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ControllerServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter pw = resp.getWriter();
+        String name = req.getParameter("name");
+        pw.println("<HTML><HEAD>");
+        pw.println("<TITLE>hello</TITLE>");
+        pw.println("</HEAD><BODY>");
+        if(name!=null)
+            pw.println("<H1> HELLO!!! " + name+" </H!>");
+        else
+            pw.println("<H1> HELLO!!! </H!>");
+        pw.println("<br><br><a href = /Komis/makeForm.html>formularz</a>");
+        pw.println("</BODY></HTML>");
+
+    }
+}
---
+++ src/main/java/pl/altkom/web/RedirectServlett.java
@@ -0,0 +1,38 @@
+package pl.altkom.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class RedirectServlett extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String str = req.getParameter("marka");
+        PrintWriter pw = resp.getWriter();
+        String name = req.getParameter("name");
+        pw.println("<HTML><HEAD>");
+        pw.println("<TITLE>hello</TITLE>");
+        pw.println("</HEAD><BODY>");
+        pw.println("<br><br><h1>wybrales samochod "+str+"</h1>");
+        pw.println("</BODY></HTML>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String str = req.getParameter("marka");
+        PrintWriter pw = resp.getWriter();
+//        String name = req.getParameter("name");
+        pw.println("<HTML><HEAD>");
+       pw.println("<TITLE>hello</TITLE>");
+        pw.println("</HEAD><BODY>");
+        pw.println("<br><br><h1>wybrales samochod "+str+"</h1>");
+        pw.println("za chwile nastapi przekierowanie na strone producenta");
+        pw.println("</BODY></HTML>");
+
+        pw.println("<meta http-equiv=\"Refresh\" content=\"5;url=http://www."+str+".com\">");
+
+    }
+}
