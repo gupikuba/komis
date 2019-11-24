b1a3cf40daa06da7bb820d1cb36ea4a2e0908f47
student <fjd@gmai.com>
2019-11-24 12:12:45 +0100

    komis poczatek

---
+++ src/main/java/pl/altkom/web/ServletKuba.java
@@ -0,0 +1,21 @@
+package pl.altkom.web;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ServletKuba extends HttpServlet {
+    @Override
+    public void service(HttpServletRequest request,
+                        HttpServletResponse response)
+            throws IOException {
+        PrintWriter pw = response.getWriter();
+        pw.println("<HTML><HEAD>");
+        pw.println("<TITLE>HelloKuba</TITLE>");
+        pw.println("</HEAD><BODY>");
+        pw.println("<H3>Hello Kuba!</H3>");
+        pw.println("</BODY></HTML>");
+    }
+}
